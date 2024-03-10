90492e84fd2fc9942007158de4efc718b16c26f1
khushishah2901 <121679622+khushishah2901@users.noreply.github.com>
2024-03-10 10:36:12 +0530

    Color Picker Page

---
+++ index.js
@@ -0,0 +1,30 @@
+let bagroudContainerEl=document.getElementById("bagroudContainer");
+let colorNameEl=document.getElementById("colorName");
+
+function firstbtn(){
+    bagroudContainerEl.style.background=" rgb(194, 183, 183)";
+    colorNameEl.textContent="#0e0e0";
+    colorNameEl.style.color="skyblue";
+
+}
+
+function secondbtn(){
+    bagroudContainerEl.style.background="#6fcf97";
+    colorNameEl.textContent="#6fcf97";
+    colorNameEl.style.color="skyblue";
+
+}
+function thirdbtn(){
+    bagroudContainerEl.style.background="#56ccf2";
+    colorNameEl.textContent="#56ccf2";
+    colorNameEl.style.color="skyblue";
+
+
+}
+
+function fourthbtn(){
+    bagroudContainerEl.style.background="#f08080";
+    colorNameEl.textContent="#f08080";
+    colorNameEl.style.color="skyblue";
+
+}
